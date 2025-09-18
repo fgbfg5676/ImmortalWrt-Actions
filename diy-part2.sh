62cfbcc505d9d2228eff800e5df1f295b491b16f
fgbfg5676 <niwo5507@gmail.com>
2025-09-18 22:40:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/192.168.1.1/10.10.10.100/g' package/base-files/files/bin/config_generate
 
 # Enable r8125 ASPM
@@ -31,12 +31,12 @@
 # wget https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/cstnet.txt -O feeds/luci/applications/luci-app-mwan3helper/root/etc/mwan3helper/othernet.txt
 
 # Change dnsproxy behavior
-# sed -i 's/--cache --cache-min-ttl=3600/--cache --cache-min-ttl=600/g' ./feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
+sed -i 's/--cache --cache-min-ttl=3600/--cache --cache-min-ttl=600/g' ./feeds/luci/applications/luci-app-turboacc/root/etc/init.d/turboacc
 
 # Convert zh-cn to zh_Hans
-#bash <( curl -sSL https://build-scripts.immortalwrt.eu.org/convert_translation.sh )
-#bash <( curl -sSL https://build-scripts.immortalwrt.eu.org/create_acl_for_luci.sh ) -a
-#rm -rf ./tmp
+bash <( curl -sSL https://build-scripts.immortalwrt.eu.org/convert_translation.sh )
+bash <( curl -sSL https://build-scripts.immortalwrt.eu.org/create_acl_for_luci.sh ) -a
+rm -rf ./tmp
 
 # Update Golang
 git clone -b master --single-branch https://github.com/immortalwrt/packages.git packages_master
